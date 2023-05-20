d9d292420efb98bbe2434c1669ded4a555e6494e
IsaacHdezA <aldoisaachernandez@outlook.com>
2023-05-20 17:03:37 -0600

    Added Image resizing if width is bigger than half of the screen's (only Windows)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+#if WIN32
+  #include <Windows.h>
+
+  inline int  getScreenWidth() { return (int) ::GetSystemMetrics(SM_CXSCREEN); }
+  inline int getScreenHeight() { return (int) ::GetSystemMetrics(SM_CYSCREEN); }
+#endif
+
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -14,10 +21,10 @@ using namespace std;
 using namespace cv;
 
 void writeCSV(string filename, Mat m) {
-   ofstream myfile;
-   myfile.open(filename.c_str());
-   myfile<< cv::format(m, cv::Formatter::FMT_CSV) << std::endl;
-   myfile.close();
+  ofstream myfile;
+  myfile.open(filename.c_str());
+  myfile<< cv::format(m, cv::Formatter::FMT_CSV) << std::endl;
+  myfile.close();
 }
 
 inline float toRad(float deg) { return (deg * M_PI)/180; }
@@ -29,144 +36,150 @@ Mat normHist(const Mat &);
 float computeHomogeneity(const Mat &);
 
 int main() {
-    const string IMG_PATH      = "./res/",
-                 IMG_FILENAME  = "test3",
-                 IMG_EXTENSION = ".jpg",
-                 IMG_FULLPATH  = IMG_PATH + IMG_FILENAME + IMG_EXTENSION;
+  const string IMG_PATH      = "./res/",
+               IMG_FILENAME  = "test3",
+               IMG_EXTENSION = ".jpg",
+               IMG_FULLPATH  = IMG_PATH + IMG_FILENAME + IMG_EXTENSION;
 
-    Mat img = imread(IMG_FULLPATH, IMREAD_GRAYSCALE);
-    // resize(img, img, Size(), 0.45, 0.45);
+  Mat img = imread(IMG_FULLPATH, IMREAD_GRAYSCALE);
 
-    // Mat img = (Mat_<uchar>(5, 5) <<
-    //           0, 255,   0,   0,   0,
-    //         255, 255, 255,   0, 255,
-    //         255, 255,   0, 255, 255,
-    //         255,   0, 255, 255, 255,
-    //           0, 255, 255,   0, 255
-    // );
+  #if WIN32
+    const int SCRN_WIDTH = getScreenWidth(), SCRN_HEIGHT = getScreenHeight();
 
-    ofstream horHomogeneityFile("./out/" + IMG_FILENAME + "_horHomogeneity.csv", ios::out),
-             verHomogeneityFile("./out/" + IMG_FILENAME + "_verHomogeneity.csv", ios::out);
+    if(img.cols > (SCRN_WIDTH / 2) || img.rows > (SCRN_HEIGHT / 2))
+      resize(img, img, Size((SCRN_WIDTH / 2), (SCRN_HEIGHT / 2)));
+  #endif
 
-    Mat horNormDifHist,
-        verNormDifHist;
+  // Mat img = (Mat_<uchar>(5, 5) <<
+  //           0, 255,   0,   0,   0,
+  //         255, 255, 255,   0, 255,
+  //         255, 255,   0, 255, 255,
+  //         255,   0, 255, 255, 255,
+  //           0, 255, 255,   0, 255
+  // );
 
-    for(int d = 1; d < img.cols; d++) {
-        horNormDifHist = normHist(computeHorDifHist(img, d, 0));
-        horHomogeneityFile << computeHomogeneity(horNormDifHist) << ((d + 1) < img.cols ? "," : "");
-    }
+  ofstream horHomogeneityFile("./out/" + IMG_FILENAME + "_horHomogeneity.csv", ios::out),
+           verHomogeneityFile("./out/" + IMG_FILENAME + "_verHomogeneity.csv", ios::out);
 
-    for(int d = 1; d < img.cols; d++) {
-        verNormDifHist = normHist(computeVerDifHist(img, d, 0));
-        verHomogeneityFile << computeHomogeneity(verNormDifHist) << ((d + 1) < img.cols ? "," : "");
-    }
+  Mat horNormDifHist,
+      verNormDifHist;
 
-    horHomogeneityFile.close();
-    verHomogeneityFile.close();
+  for(int d = 1; d < img.cols; d++) {
+      horNormDifHist = normHist(computeHorDifHist(img, d, 0));
+      horHomogeneityFile << computeHomogeneity(horNormDifHist) << ((d + 1) < img.cols ? "," : "");
+  }
 
-    // Creating plots
-    system(("python plot.py " + IMG_FILENAME + " --save").c_str());
-    // system(("python plot.py " + IMG_FILENAME + " --show").c_str());
-    // system(("python plot.py " + IMG_FILENAME + " --save-and-show").c_str());
+  for(int d = 1; d < img.cols; d++) {
+      verNormDifHist = normHist(computeVerDifHist(img, d, 0));
+      verHomogeneityFile << computeHomogeneity(verNormDifHist) << ((d + 1) < img.cols ? "," : "");
+  }
 
-    imshow(IMG_FILENAME, img);
-    moveWindow(IMG_FILENAME, 0, 0);
+  horHomogeneityFile.close();
+  verHomogeneityFile.close();
 
-    Mat plot = imread("./out/img/" + IMG_FILENAME + "_homogeneity.png");
-    resize(plot, plot, Size(), 0.5, 0.5);
-    imshow(IMG_FILENAME + " homogeneity", plot);
-    moveWindow(IMG_FILENAME + " homogeneity", img.cols, 0);
+  // Creating plots
+  system(("python plot.py " + IMG_FILENAME + " --save").c_str());
+  // system(("python plot.py " + IMG_FILENAME + " --show").c_str());
+  // system(("python plot.py " + IMG_FILENAME + " --save-and-show").c_str());
 
-    waitKey();
-    destroyAllWindows();
+  imshow(IMG_FILENAME, img);
+  moveWindow(IMG_FILENAME, 0, 0);
 
-    return 0;
+  Mat plot = imread("./out/img/" + IMG_FILENAME + "_homogeneity.png");
+  resize(plot, plot, Size(), 0.5, 0.5);
+  imshow(IMG_FILENAME + " homogeneity", plot);
+  moveWindow(IMG_FILENAME + " homogeneity", img.cols, 0);
+
+  waitKey();
+  destroyAllWindows();
+
+  return 0;
 }
 
 Mat computeSumHist(const Mat &src, const int D, const int THETA) {
-    Mat out;
+  Mat out;
 
-    if(src.empty() || src.channels() > 1 || !src.data) {
-        cout << "computeSumHist(): Image is empty or should be grayscale" << endl;
-        return out;
-    }
-
-    out = Mat::zeros(1, 511, CV_16UC1);
-    for(int i = 0; i < src.rows; i++) {
-        uchar *row = (uchar *) src.ptr<uchar>(i);
-        for(int j = 0; j < (src.cols - D); j++)
-            ++out.at<ushort>(0, (row[j] + row[j + D]));
-    }
-
+  if(src.empty() || src.channels() > 1 || !src.data) {
+    cout << "computeSumHist(): Image is empty or should be grayscale" << endl;
     return out;
+  }
+
+  out = Mat::zeros(1, 511, CV_16UC1);
+  for(int i = 0; i < src.rows; i++) {
+    uchar *row = (uchar *) src.ptr<uchar>(i);
+    for(int j = 0; j < (src.cols - D); j++)
+      ++out.at<ushort>(0, (row[j] + row[j + D]));
+  }
+
+  return out;
 }
 
 Mat computeHorDifHist(const Mat &src, const int D, const int THETA) {
-    Mat out;
-    const float ANGLE = toRad(THETA + 90);
-    // cout << THETA << "deg to rad: " << ANGLE << "rad" << '\n'
-    //      << "\tx = " << cos(ANGLE) << "\n"
-    //      << "\ty = " << sin(ANGLE) << "\n";
+  Mat out;
+  const float ANGLE = toRad(THETA + 90);
+  // cout << THETA << "deg to rad: " << ANGLE << "rad" << '\n'
+  //      << "\tx = " << cos(ANGLE) << "\n"
+  //      << "\ty = " << sin(ANGLE) << "\n";
 
-    if(src.empty() || src.channels() > 1 || !src.data) {
-        cout << "computeSumHist(): Image is empty or should be grayscale" << endl;
-        return out;
-    }
-
-    out = Mat::zeros(1, 511, CV_16UC1);
-    for(int r = 0; r < src.rows; r++) {
-        uchar *row = (uchar *) src.ptr<uchar>(r);
-        for(int c = 0; c < (src.cols - D); c++)
-            ++out.at<ushort>(0, (row[c] - row[c + D]) + 255);
-    }
-
+  if(src.empty() || src.channels() > 1 || !src.data) {
+    cout << "computeSumHist(): Image is empty or should be grayscale" << endl;
     return out;
+  }
+
+  out = Mat::zeros(1, 511, CV_16UC1);
+  for(int r = 0; r < src.rows; r++) {
+    uchar *row = (uchar *) src.ptr<uchar>(r);
+    for(int c = 0; c < (src.cols - D); c++)
+      ++out.at<ushort>(0, (row[c] - row[c + D]) + 255);
+  }
+
+  return out;
 }
 
 Mat computeVerDifHist(const Mat &src, const int D, const int THETA) {
-    Mat out;
+  Mat out;
 
-    if(src.empty() || src.channels() > 1 || !src.data) {
-        cout << "computeSumHist(): Image is empty or should be grayscale" << endl;
-        return out;
-    }
-
-    out = Mat::zeros(1, 511, CV_16UC1);
-    for(int c = 0; c < src.cols; c++) {
-        for(int r = 0; r < (src.rows - D); r++)
-            ++out.at<ushort>(0, (src.at<uchar>(r, c) - src.at<uchar>(r + D, c)) + 255);
-    }
-
+  if(src.empty() || src.channels() > 1 || !src.data) {
+    cout << "computeSumHist(): Image is empty or should be grayscale" << endl;
     return out;
+  }
+
+  out = Mat::zeros(1, 511, CV_16UC1);
+  for(int c = 0; c < src.cols; c++) {
+    for(int r = 0; r < (src.rows - D); r++)
+      ++out.at<ushort>(0, (src.at<uchar>(r, c) - src.at<uchar>(r + D, c)) + 255);
+  }
+
+  return out;
 }
 
 Mat normHist(const Mat &hist) {
-    Mat out;
+  Mat out;
 
-    if(hist.empty() || hist.channels() > 1 || !hist.data || hist.rows > 1) {
-        cout << "normHist: Histogram should be one row only" << endl;
-        return out;
-    }
-
-    out = hist.clone();
-    out.convertTo(out, CV_32FC1);
-
-    int totSum = sum(out)[0];
-    out = out / totSum;
-
+  if(hist.empty() || hist.channels() > 1 || !hist.data || hist.rows > 1) {
+    cout << "normHist: Histogram should be one row only" << endl;
     return out;
+  }
+
+  out = hist.clone();
+  out.convertTo(out, CV_32FC1);
+
+  int totSum = sum(out)[0];
+  out = out / totSum;
+
+  return out;
 }
 
 float computeHomogeneity(const Mat &difHist) {
-    if(difHist.empty() || difHist.channels() > 1 || !difHist.data || difHist.rows > 1) {
-        cout << "normHist: Histogram should be one row only" << endl;
-        return -1;
-    }
+  if(difHist.empty() || difHist.channels() > 1 || !difHist.data || difHist.rows > 1) {
+    cout << "normHist: Histogram should be one row only" << endl;
+    return -1;
+  }
 
-    float homogeneity = 0;
-    for(int i = 0; i < difHist.cols; i++)
-        // homogeneity += (1.0/(1 + ((i - 255) * (i - 255)))) * difHist.at<float>(0, i);
-           homogeneity += (1.0/(1 + ((i - 255) * (i - 255)))) * difHist.at<float>(0, i);
+  float homogeneity = 0;
+  for(int i = 0; i < difHist.cols; i++)
+    // homogeneity += (1.0/(1 + ((i - 255) * (i - 255)))) * difHist.at<float>(0, i);
+    homogeneity += (1.0/(1 + ((i - 255) * (i - 255)))) * difHist.at<float>(0, i);
 
-    return homogeneity;
+  return homogeneity;
 }
